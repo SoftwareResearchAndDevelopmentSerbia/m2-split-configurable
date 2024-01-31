eb2cb079ebf9decfe5beab015358b605f8635e88
Milan Cvetković <software.rnd.com@gmail.com>
2024-01-31 23:05:37 +0100

    SplitConfigurable module added and activated

---
+++ app/code/SoftwareResearchAndDevelopment/SplitConfigurable/registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright © Software Research and Development. All rights reserved.
+ * See LICENSE_SOFTWARE_RESEARCH_AND_DEVELOPMENT.txt for license details.
+ */
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'SoftwareResearchAndDevelopment_SplitConfigurable',
+    __DIR__
+);
